c9879291ecc4dd42367864737b621a25b6a81b70
AhmedEl-Gammal <ahmedkhaledmohamedelgammal@gmail.com>
2025-05-08 03:40:07 +0300

    Initial commit

---
+++ Mips16/syn/scripts/synthesis.tcl
@@ -0,0 +1,7 @@
+####################################
+# ============ Setup ============= #
+####################################
+# --- Libraries
+set_app_var search_path "/home/ICer/Downloads/Lib/synopsys/models"
+set_app_var target_library "saed90nm_max.db"  
+set_app_var link_library "* $target_library"
